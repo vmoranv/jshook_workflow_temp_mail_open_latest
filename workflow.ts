5f8c98db751f7cc8d2af53f96d31f73cc65dfb87
vmoranv <1761886648@qq.com>
2026-04-01 18:52:06 +0800

    initial: scaffold from jshookmcp workflows/temp-mail-open-latest-r2

---
+++ workflow.ts
@@ -0,0 +1,4 @@
+// TODO: Convert from plain JS to TypeScript SDK format
+// See jshook_workflow_template/workflow.ts for reference
+// Interim: dist/workflow.js contains the working plain JS version
+export {};
